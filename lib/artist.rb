5f7c005d8e75e05f46ab6c3135ff06f1c737e3f0
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-29 18:44:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -37,13 +37,7 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    songs.each do |song|
-      add_song(song)
-    end
-  end
-
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
+    songs.each {|song|add_song(song)}
   end
 
   # def to_param
